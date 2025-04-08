a49524e908a9817e00fa1a3105c7e2c06e79bbf2
Wesley Luiz <wesleylm03@outlook.com>
2025-04-08 14:42:04 -0300

    Atividade01 da Aula 04 - 08.04.2025

---
+++ atividade01aula04.py
@@ -0,0 +1,29 @@
+mes = int(input("Digite um número: "))
+
+if mes == 1:
+    print("Janeiro")
+elif mes == 2:
+    print("Fevereiro")
+elif mes == 3:
+    print("Março")
+elif mes == 4:
+    print("Abril")
+elif mes == 5:
+    print("Maio")
+elif mes == 6:
+    print("Junho")
+elif mes == 7:
+    print("Julho")
+elif mes == 8:
+    print("Agosto")
+elif mes == 9:
+    print("Setembro")
+elif mes == 10:
+    print("Outubro")
+elif mes == 11:
+    print("Novembro")
+elif mes == 12:
+    print("Dezembro")
+else:
+    print("Digite um número válido.")
+
